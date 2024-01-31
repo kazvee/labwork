ed87cbd7c9203d8fdee4d3f84ad3ae86aa92bf74
kazvee <kazveedev@gmail.com>
2024-01-30 19:59:03 -0700

    Refactor find largest of all numbers to initialize largestNumber with negative infinity

--- class_5/largest_of_all_numbers.py
+++ class_5/largest_of_all_numbers.py
@@ -1,9 +1,9 @@
-largestNumber = 0
-numberToCheck = int(input("Enter a positive number, or enter `-1` to stop: "))
+largestNumber = float('-inf')
+numberToCheck = int(input("Enter a number, or enter `-1` to stop: "))
 
 while numberToCheck != -1:
     if numberToCheck > largestNumber:
         largestNumber = numberToCheck
-    numberToCheck = int(input("Enter a positive number, or enter `-1` to stop: "))
+    numberToCheck = int(input("Enter a number, or enter `-1` to stop: "))
 
-print("The largest number is:", largestNumber)
+print("The largest number is: ", largestNumber)
