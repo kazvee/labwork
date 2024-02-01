bd0da1de4b790882a60c3be71965c5051dba2d4c
kazvee <kazveedev@gmail.com>
2024-02-01 16:18:30 -0700

    Refactor smallest of all numbers exercise

--- class_5/smallest_of_all_numbers.py
+++ class_5/smallest_of_all_numbers.py
@@ -1,9 +1,12 @@
-smallestNumber = float('inf')
-numberToCheck = int(input("Enter a number, or enter `-1` to stop: "))
+smallest_number = float('inf')
+number_to_check = input("Enter a number, or enter '$' to stop: ")
 
-while numberToCheck != -1:
-    if numberToCheck < smallestNumber:
-        smallestNumber = numberToCheck
-    numberToCheck = int(input("Enter a number, or enter `-1` to stop: "))
+while number_to_check != '$':
+    number_to_check = int(number_to_check)
 
-print("The smallest number is: ", smallestNumber)
+    if number_to_check < smallest_number:
+        smallest_number = number_to_check
+
+    number_to_check = input("Enter a number, or enter '$' to stop: ")
+
+print("The smallest number is:", smallest_number)
